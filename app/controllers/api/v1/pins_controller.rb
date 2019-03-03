f9f92cd2b2979e8745d0a83bce5a7bf1b78776cb
Stiven Diaz <david.stiven.diaz@accenture.com>
2019-03-03 10:32:49 -0500

    add secure in api

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,19 +1,35 @@
 class Api::V1::PinsController < ApplicationController
-  def index
-    render json: Pin.all.order('created_at DESC')
-  end
-
-  def create
-    pin = Pin.new(pin_params)
-    if pin.save
-      render json: pin, status: 201
-    else
-      render json: { errors: pin.errors }, status: 422
-    end
-  end
-
-  private
-    def pin_params
-      params.require(:pin).permit(:title, :image_url)
-    end
-end
+  before_action :restrict_access
+ 
+   def index
+     render json: Pin.all.order('created_at DESC')
+   end
+ 
+   def create
+     pin = Pin.new(pin_params)
+     if pin.save
+       render json: pin, status: 201
+     else
+       render json: { errors: pin.errors }, status: 422
+     end
+   end
+ 
+   private
+     def pin_params
+       params.require(:pin).permit(:title, :image_url)
+     end
+ 
+      def restrict_access
+        email = request.headers['HTTP_X_USER_EMAIL']
+        token = request.headers['HTTP_X_API_TOKEN']
+        user = User.find_by_email(email) 
+       if user      
+          unless user.api_token == token
+           head 401
+          end
+        else
+         head 401
+        end
+      end    
+ 
+ end
